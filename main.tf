7acf5d020d84ae3480835a9425533beb46771908
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-08-15 22:02:25 +0100

    first draft

--- main.tf
+++ main.tf
@@ -1,36 +1,80 @@
-locals {
-  policy_rule_collection_name = var.fw_rule_collection_group_name != "" ? var.fw_rule_collection_group_name : "${var.fw_policy_name}-rcg"
-}
-
 resource "azurerm_firewall_policy_rule_collection_group" "rule_collection_group" {
-  name               = local.policy_rule_collection_name
+  name               = var.fw_rule_collection_group_name
   firewall_policy_id = var.fw_policy_id
   priority           = var.priority
 
   dynamic "application_rule_collection" {
+    for_each = var.application_rule_collections
+    content {
+      name     = application_rule_collection.value.name
+      priority = application_rule_collection.value.priority
+      action   = application_rule_collection.value.action
 
+      dynamic "rule" {
+        for_each = application_rule_collection.value.rules
+        content {
+          name              = rule.value.name
+          source_addresses  = lookup(rule.value, "source_addresses", null)
+          destination_fqdns = lookup(rule.value, "destination_fqdns", null)
+
+          dynamic "protocols" {
+            for_each = rule.value.protocols
+            content {
+              type = protocols.value.type
+              port = protocols.value.port
+            }
+          }
+        }
+      }
+    }
   }
-
 
   dynamic "network_rule_collection" {
-      for_each = var.network_rule_collection
-      content {
-          name                  = network_rule_collection.value.name                                   # string
-          description           = lookup(network_rule_collection.value, "description", null)           # string
-          source_addresses      = lookup(network_rule_collection.value, "source_addresses", null)      # list EXAMPLE - ["10.0.0.0/16",]
-          source_ip_groups      = lookup(network_rule_collection.value, "source_ip_groups", null)      # list
-          destination_ports     = network_rule_collection.value.destination_ports                      # list EXAMPLE - ["53",]
-          destination_fqdns     = lookup(network_rule_collection.value, "destination_fqdns", null)     # list (DNS Proxy must be enabled to use FQDNs.)
-          destination_ip_groups = lookup(network_rule_collection.value, "destination_ip_groups", null) # list
-          destination_addresses = lookup(network_rule_collection.value, "destination_addresses", null) # list EXAMPLE -  ["8.8.8.8","8.8.4.4",]
-          protocols             = network_rule_collection.value.protocols                              # list EXAMPLE - ["TCP","UDP",]
-      }
-  }
+    for_each = var.network_rule_collections
+    content {
+      name     = network_rule_collection.value.name
+      priority = network_rule_collection.value.priority
+      action   = network_rule_collection.value.action
 
+      dynamic "rule" {
+        for_each = network_rule_collections.value.rules
+        content {
+          name                  = rule.value.name
+          protocols             = rule.value.protocols                              # list
+          destination_ports     = rule.value.destination_ports                      # list - Required
+          source_addresses      = lookup(rule.value, "source_addresses", null)      # list
+          source_ip_groups      = lookup(rule.value, "source_ip_groups", null)      # list Specifies a list of source IP groups.
+          destination_addresses = lookup(rule.value, "destination_addresses", null) # list - ["192.168.1.1", "192.168.1.2"]
+          destination_ip_groups = lookup(rule.value, "destination_ip_groups", null) # list of destination IP groups.
+          destination_fqdns     = lookup(rule.value, "destination_fqdns", null)     # list of destination IP groups.
+        }
+      }
+    }
+  }
 
   dynamic "nat_rule_collection" {
+    for_each = var.nat_rule_collections
+    content {
+      name     = nat_rule_collection.value.name
+      priority = nat_rule_collection.value.priority
+      action   = nat_rule_collection.value.action
 
+      dynamic "rule" {
+        for_each = nat_rule_collections.value.rules
+        content {
+          name                = rule.value.name
+          protocols           = rule.value.protocols                         # list
+          source_addresses    = lookup(rule.value, "source_addresses", null) # list
+          destination_address = lookup(rule.value, "destination_addresses", null)
+          destination_ports   = lookup(rule.value, "destination_ports", null)  # list
+          source_ip_groups    = lookup(rule.value, "source_ip_groups", null)   # list of source IP groups.
+          translated_address  = lookup(rule.value, "translated_address", null) # Specifies the translated address
+          translated_port     = lookup(rule.value, "translated_port", null)    # Specifies the translated port.
+        }
+      }
+    }
   }
 
-
 }
+
+
