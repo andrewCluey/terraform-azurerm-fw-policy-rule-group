89beb2020cca42b48aecbae0d007e947bf74e777
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-08-15 22:06:29 +0100

    .

---
+++ examples/main.tf
@@ -0,0 +1,12 @@
+
+
+
+
+
+module = "default_policy_rule_collection" {
+    source = "../"
+    
+    fw_rule_collection_group_name = "default"
+    priority                      = "500"
+    fw_policy_id                  = 
+}
